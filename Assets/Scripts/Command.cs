8527fb0a2c2abe322e558511bcfb13a792db7a52
Oleracea <kyleyma3@gmail.com>
2022-11-13 15:49:57 -0700

    grounded and respawn

--- Assets/Scripts/Command.cs
+++ Assets/Scripts/Command.cs
@@ -8,13 +8,29 @@ abstract class Command : MonoBehaviour {
 }
 
 class Jump : Command {
-    new public void execute() {
-
+    new public void execute(float moveHorizontal) {
+        // if (moveHorizontal != 0f) {
+        //     if(moveHorizontal > 0f){
+        //         transform.localScale = new Vector3(1, 1, 1);
+        //     } else if(moveHorizontal < 0f){
+        //         transform.localScale = new Vector3(-1, 1, 1);
+        //     }
+        //     rb2D.AddForce(new Vector2(moveHorizontal * moveSpeed, 0f), ForceMode2D.Impulse);
+        // }
     }
 }
 
 class Run : Command {
-    new public void execute() {
 
+    new public void execute(float verticalHorizontal) {
+        // if (moveVertical > 0f && IsGrounded()) {
+        //     rb2D.AddForce(new Vector2(0f, moveVertical * jumpForce), ForceMode2D.Impulse);
+        // }
     }
+
+    // private bool IsGrounded(){
+    //     // float buffer = .01f;
+    //     // RaycastHit2D raycastHit = Physics2D.Raycast(cc2d.bounds.center, Vector2.down, cc2d.bounds.extents.y + buffer, groundLayer);
+    //     // return (raycastHit.collider != null);
+    // }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,19 +5,30 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     private Rigidbody2D rb2D;
+    private CapsuleCollider2D cc2d;
     private float moveSpeed;
     private float jumpForce;
     private bool isJumping;
     private float moveHorizontal;
     private float moveVertical;
-    private LayerMask groundLayer;
+    private bool grounded;
+    Jump jump;
+    Run run;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private LayerMask trapLayer;
+    [SerializeField] private Transform respawn;
+
     
     // Start is called before the first frame update
     void Start() {
+        jump = new Jump();
+        run = new Run();
         moveSpeed = 1f;
-        jumpForce = 10f;
+        jumpForce = 20f;
         isJumping = false;
         rb2D = gameObject.GetComponent<Rigidbody2D>();
+        cc2d = gameObject.GetComponent<CapsuleCollider2D>();
+        grounded = true;
     }
 
     // Update is called once per frame
@@ -28,11 +39,32 @@ public class PlayerController : MonoBehaviour {
 
     void FixedUpdate() {
         if (moveHorizontal != 0f) {
+            if(moveHorizontal > 0f){
+                transform.localScale = new Vector3(1, 1, 1);
+            } else if(moveHorizontal < 0f){
+                transform.localScale = new Vector3(-1, 1, 1);
+            }
             rb2D.AddForce(new Vector2(moveHorizontal * moveSpeed, 0f), ForceMode2D.Impulse);
         }
 
-        if (!isJumping && moveVertical > 0f) {
+        if (!isJumping && moveVertical > 0f && IsGrounded()) {
             rb2D.AddForce(new Vector2(0f, moveVertical * jumpForce), ForceMode2D.Impulse);
         }
+        if(Kill()){
+            Debug.Log("trapped");
+            rb2D.position = respawn.position;
+        }
+    }
+
+    private bool IsGrounded(){
+        float buffer = .01f;
+        RaycastHit2D raycastHit = Physics2D.Raycast(cc2d.bounds.center, Vector2.down, cc2d.bounds.extents.y + buffer, groundLayer);
+        return (raycastHit.collider != null);
+    }
+
+    private bool Kill(){
+        float buffer = .01f;
+        RaycastHit2D raycastHit = Physics2D.BoxCast(cc2d.bounds.center, cc2d.bounds.size, 0f, Vector2.down, buffer, trapLayer);
+        return (raycastHit.collider != null);
     }
 }
