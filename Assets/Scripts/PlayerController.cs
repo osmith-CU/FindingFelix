c6e973e8668eefc823b07b0df46161cac76061e7
Oleracea <kyleyma3@gmail.com>
2022-11-13 18:09:25 -0700

    level load, double jump is so fucked idk

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour {
 
@@ -8,24 +9,30 @@ public class PlayerController : MonoBehaviour {
     private CapsuleCollider2D cc2d;
     private float moveSpeed;
     private float jumpForce;
-    private bool isJumping;
+    private bool isFalling;
     private float moveHorizontal;
     private float moveVertical;
     private bool grounded;
+    private bool hasJumpedOnce;
+    public int loadInt;
+    public string loadString;
+    public bool useInt;
     Jump jump;
     Run run;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private LayerMask trapLayer;
+    [SerializeField] private LayerMask levelLayer;
     [SerializeField] private Transform respawn;
 
     
     // Start is called before the first frame update
     void Start() {
-        jump = new Jump();
-        run = new Run();
+        useInt = false;
+        loadInt = 1;
+        hasJumpedOnce = false;
         moveSpeed = 1f;
-        jumpForce = 20f;
-        isJumping = false;
+        jumpForce = 25f;
+        isFalling = false;
         rb2D = gameObject.GetComponent<Rigidbody2D>();
         cc2d = gameObject.GetComponent<CapsuleCollider2D>();
         grounded = true;
@@ -38,6 +45,9 @@ public class PlayerController : MonoBehaviour {
     }
 
     void FixedUpdate() {
+        if(IsGrounded()){
+            hasJumpedOnce = false;            
+        }
         if (moveHorizontal != 0f) {
             if(moveHorizontal > 0f){
                 transform.localScale = new Vector3(1, 1, 1);
@@ -47,12 +57,28 @@ public class PlayerController : MonoBehaviour {
             rb2D.AddForce(new Vector2(moveHorizontal * moveSpeed, 0f), ForceMode2D.Impulse);
         }
 
-        if (!isJumping && moveVertical > 0f && IsGrounded()) {
+        if (moveVertical > 0f && (IsGrounded() || !hasJumpedOnce)) {
+            Debug.Log(hasJumpedOnce);
+            Debug.Log(!hasJumpedOnce);
+            hasJumpedOnce = !hasJumpedOnce;
             rb2D.AddForce(new Vector2(0f, moveVertical * jumpForce), ForceMode2D.Impulse);
+            
+            Debug.Log(hasJumpedOnce);
+        }
+        // if(hasJumpedOnce == true){
+        //     rb2D.AddForce(new Vector2(0f, moveVertical * jumpForce), ForceMode2D.Impulse);
+        //     hasJumpedOnce = false;
+        // }
+        if(moveVertical < 0f){
+            isFalling = true;
         }
         if(Kill()){
             Debug.Log("trapped");
             rb2D.position = respawn.position;
+        }
+        if(loadLevelCheck()){
+            Debug.Log("nextLevel");
+            loadNextLevel();
         }
     }
 
@@ -66,5 +92,15 @@ public class PlayerController : MonoBehaviour {
         float buffer = .01f;
         RaycastHit2D raycastHit = Physics2D.BoxCast(cc2d.bounds.center, cc2d.bounds.size, 0f, Vector2.down, buffer, trapLayer);
         return (raycastHit.collider != null);
+    }
+
+    private bool loadLevelCheck(){
+        float buffer = .01f;
+        RaycastHit2D raycastHit = Physics2D.BoxCast(cc2d.bounds.center, cc2d.bounds.size, 0f, Vector2.down, buffer, levelLayer);
+        return raycastHit.collider != null;
+    }
+
+    private void loadNextLevel(){
+        SceneManager.LoadScene(loadInt);
     }
 }
